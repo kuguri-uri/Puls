5cef469f86ffc918d032bd4104c7539c31b39ddb
Vera <mokeeva.vk@phystech.edu>
2022-08-16 18:16:11 +0300

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -32,4 +32,14 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    $('.button_mini').on('click', function() {
+        $('.overlay, #order').fadeIn('slow');
+    });
   });
